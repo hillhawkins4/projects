5c0ab76fdd6e8a2c87363a2b07cf37ed05831387
hillhawkins4 <hillhawkins4@gmail.com>
2026-04-13 18:15:50 -0500

    Add files via upload

---
+++ bmicalc.py
@@ -0,0 +1,11 @@
+# quick bmi calculator
+
+height = 69
+theight = height * 0.0254  # convert inches to meters
+
+mass = 145
+tmass = mass * 0.45359237  # convert pounds to kg
+
+bmi = tmass / (theight ** 2)
+
+print(bmi)
